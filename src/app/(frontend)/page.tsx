81a11895a316d18544111aba711f84a963f3291d
Tu Nguyen <tuna9x.it@gmail.com>
2024-12-03 14:29:24 +0700

    Add code

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,26 +3,29 @@ import CarSlider from '@/components/carSlider/component.client'
 import ProductListsByCategory from '@/components/productListsByCategory/component.client'
 import DefaultLayout from '@/layouts/DefaultLayout/layout'
 import type { NextPage } from 'next'
-
+import BannerPromo from '@/components/ui/banner-promo'
 import React from 'react'
 
 const Page: NextPage = () => {
   return (
-    <DefaultLayout>
-      <div className="container">
-        <h1 className="text-center text-4xl font-bold">
-          Chợ phụ kiện ôtô & Đồ chơi xe điện & Sạc điện & Nội - ngoại thất ôtô
-        </h1>
-      </div>
+    <>
+      <BannerPromo />
+      <DefaultLayout>
+        <div className="container">
+          <h1 className="text-center text-4xl font-bold">
+            Chợ phụ kiện ôtô & Đồ chơi xe điện & Sạc điện & Nội - ngoại thất ôtô
+          </h1>
+        </div>
 
-      <div className="container p-8 text-center font-medium">
-        <ProductListsByCategory categoryName="vehicle" />
-      </div>
+        <div className="container p-8 text-center font-medium">
+          <ProductListsByCategory categoryName="vehicle" />
+        </div>
 
-      {/* <div className="container p-8 text-center font-medium">
+        {/* <div className="container p-8 text-center font-medium">
         <CarSlider />
       </div> */}
-    </DefaultLayout>
+      </DefaultLayout>
+    </>
   )
 }
 
---
+++ src/components/ui/banner-promo.js
@@ -0,0 +1,13 @@
+'use client'
+
+import Container from './container'
+
+export default function BannerPromotion() {
+  return (
+    <div>
+      <Container>
+        <p className="text-medium text-center font-bold">Go live soon!</p>
+      </Container>
+    </div>
+  )
+}
